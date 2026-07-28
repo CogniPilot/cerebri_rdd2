a17675d383a4fd9902fb6973515d191de5e33200
anacforcelli <anacforcelli@gmail.com>
2026-07-28 11:00:17 -0300

    Add generic PWM device support

--- src/control_io.c
+++ src/control_io.c
@@ -18,7 +18,7 @@ LOG_MODULE_DECLARE(rdd2, LOG_LEVEL_INF);
 
 #define RC_NODE    DT_ALIAS(rc)
 #define IMU_NODE   DT_ALIAS(imu0)
-#define DSHOT_NODE DT_ALIAS(motors)
+#define MOTOR_NODE DT_ALIAS(motors)
 #define GNSS_NODE  DT_ALIAS(gnss)
 
 static bool ready_or_log(const struct device *dev, const char *name)
@@ -35,14 +35,10 @@ int rdd2_control_io_init(void)
 {
 	const struct device *const rc_dev = DEVICE_DT_GET(RC_NODE);
 	const struct device *const imu_dev = DEVICE_DT_GET(IMU_NODE);
-	const struct device *const dshot_dev = DEVICE_DT_GET(DSHOT_NODE);
+	const struct device *const motor_dev = DEVICE_DT_GET(MOTOR_NODE);
 	const struct device *const gnss_dev = DEVICE_DT_GET_OR_NULL(GNSS_NODE);
 
 	rdd2_rc_input_init();
-
-	if (!ready_or_log(dshot_dev, "dshot")) {
-		return -ENODEV;
-	}
 
 	ready_or_log(rc_dev, "rc");
 	ready_or_log(imu_dev, "imu");
@@ -50,11 +46,25 @@ int rdd2_control_io_init(void)
 	if (gnss_dev != NULL && device_is_ready(gnss_dev)) {
 		LOG_INF("gnss path ready");
 	}
+#if defined(CONFIG_RDD2_DSHOT)
+	if (!ready_or_log(motor_dev, "dshot")) {
+		return -ENODEV;
+	}
 
-	if (nxp_flexio_dshot_channel_count(dshot_dev) != 4U) {
+	if (nxp_flexio_dshot_channel_count(motor_dev) != 4U) {
 		LOG_ERR("expected 4 dshot channels");
 		return -EINVAL;
 	}
+#elif defined(CONFIG_PWM)
+	if (!device_is_ready(motor_dev)) {
+		LOG_ERR("PWM device not found!");
+		return -EINVAL;
+	}
+#else
+
+#pragma message( "Using Virtual Output" )
+
+#endif
 
 	return rdd2_imu_stream_init();
 }
